0db89e63dc5d454dff545344268f2bd4b4696e75
Johann Beykirch <beykirchjohann@gmail.com>
2022-12-05 17:24:25 +0100

    world.js erstellt

--- Projekt - Escape_The_Tide/Spiel/js/character.js
+++ Projekt - Escape_The_Tide/Spiel/js/character.js
@@ -1,91 +1,87 @@
-const { applyVelocityToPosition, Velocity } = require("./physics");
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
 
-export class CharacterMeta {
-    constructor(movements_array, movement_delay, position, velocity) {
-        this._movements_array = movements_array;
-        this._position = position;
-        this._velocity = velocity;
-        this._movement_delay = movement_delay;
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'img/test2.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'gethit',
+        frames: 4,
     }
-
-    clone() {
-        return new CharacterMeta(
-            [...this._movements_array],
-            this._movement_delay,
-            this._position.clone(),
-            this._velocity.clone()
-        );
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
     }
-}
-
-export class Character {
-    constructor(character_meta) {
-        this._character_meta = character_meta;
-        this._tick_counter = 0;
-        this._movement_delay_counter = 0;
+    for (let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
     }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
-    tick() {
-        this._movement_delay_counter++;
-        if (this._movement_delay_counter > this._character_meta._movement_delay) {
-            this._movement_delay_counter = 0;
-            this._tick_counter += 1;
-            this.set_position(applyVelocityToPosition(this._character_meta._position, this._character_meta._velocity));
-        }
-    }
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
 
-    get_layout() {
-        return this._character_meta._movements_array[this._tick_counter % this._character_meta._movements_array.length];
-    }
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
-    get_position() {
-        return this._character_meta._position;
-    }
-
-    set_position(new_position) {
-        this._character_meta._position = new_position;
-    }
-
-    get_velocity() {
-        return this._character_meta._velocity;
-    }
-}
-
-export class AllocatorCharacterArray {
-    constructor() {
-        this._character_array = [];
-    }
-
-    add_character(character_meta, probability) {
-        this._character_array.push([character_meta, probability]);
-        return this;
-    }
-}
-
-export class CharacterAllocator {
-    constructor(allocator_character_array, min_gap, gap_random_max) {
-        this._allocator_character_array = allocator_character_array;
-        this._min_gap = min_gap;
-        this._gap_random_max = gap_random_max;
-        this._pending_gap = 0;
-    }
-
-    tick() {
-        this._pending_gap -= 1;
-    }
-
-    get_character() {
-        if (this._pending_gap > 0) {
-            return false;
-        }
-
-        const RANDOM = Math.random();
-        for (let i = 0; i < this._allocator_character_array._character_array.length; i++) {
-            if (RANDOM >= this._allocator_character_array._character_array[i][1]) {
-                const RANDOM_CHARACTER = new Character(this._allocator_character_array._character_array[i][0].clone());
-                this._pending_gap = this._min_gap + Math.ceil(Math.random() * this._gap_random_max);
-                return RANDOM_CHARACTER;
-            }
-        }
-    }
-}
+    gameFrame++;
+    requestAnimationFrame(animate);
+};
+animate();
