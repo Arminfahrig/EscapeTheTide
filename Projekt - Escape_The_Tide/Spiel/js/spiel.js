997a3ef465b48156cc86d480f207d51f0369db20
Johann Beykirch <beykirchjohann@gmail.com>
2022-12-07 22:53:35 +0100

    score, steigernde Geschwindigkeit, meine Nerven fertig Hindernisse und Hitboxen angefangen

--- Projekt - Escape_The_Tide/Spiel/js/spiel.js
+++ Projekt - Escape_The_Tide/Spiel/js/spiel.js
@@ -3,6 +3,10 @@ window.addEventListener('load', function (){
     const ctx = canvas.getContext('2d')
     canvas.width = 1200
     canvas.height = 675
+    let obstecles = []
+    let score = 0
+    let speed = 1 + score * 0.01
+    let gameOver = false
 
     class InputHandler {
         constructor(){
@@ -12,13 +16,13 @@ window.addEventListener('load', function (){
                     && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key)
                 }
-                console.log(e.key, this.keys)
+                //console.log(e.key, this.keys)
             })
             window.addEventListener('keyup', e => {
                 if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight'){
                     this.keys.splice(this.keys.indexOf(e.key), 1)
                 }
-                console.log(e.key, this.keys)
+                //console.log(e.key, this.keys)
             })
         }
     }
@@ -31,26 +35,50 @@ window.addEventListener('load', function (){
             this.playerheight = 100
             this.width = 840
             this.height = 840
-            this.x = 0
+            this.x = 300
             this.y = this.gameHeight - this.playerheight
             this.image = document.getElementById('playerImage')
-            this.frameX = 1
+            this.frameX = 0
+            this.maxFrame = 1
             this.frameY = 0
+            this.fps = 10
+            this.frameTimer = 0
+            this.frameInterval = 1000/this.fps
             this.speed = 0
             this.vy = 0
             this.weight = 1
         }
         draw(context){
+            context.strokeStyle = 'white'
+            context.strokeRect(this.x, this.y - 75, this.playerwidth, this.playerheight)
             //context.fillStyle = 'white'
             //context.fillRect(this.x, this.y, this.playerwidth, this.playerheight)
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y - 75, this.playerwidth, this.playerheight)
         }
-        update(input){
+        update(input, deltaTime, obstecles){
+            //colliison detection
+            obstecles.forEach(obstecle => {
+                const dx = obstecle.x - this.x - this.playerwidth
+                const dy = obstecle.y - 126 - this.y + this.playerheight
+                console.log(dx, dy)
+                if (dx <= 0 && dy <= 0) {
+                    gameOver = true
+                }
+            })
+            //sprite animation
+            if(this.frameTimer > this.frameInterval){
+                if(this.frameX >= this.maxFrame) this.frameX = 0
+                else this.frameX++
+                this.frameTimer = 0
+            } else{
+                this.frameTimer += deltaTime
+            }
+            //controls
             if (input.keys.indexOf('ArrowRight') > -1){
-                this.speed = 6
+                this.speed = speed * 5
                 this.frameY = 1
             } else if(input.keys.indexOf('ArrowLeft') > -1){
-                this.speed = -6
+                this.speed = -speed * 5
                 this.frameY = 2
             } else if(input.keys.indexOf('ArrowUp') === 0 && this.onGround()){
                 this.vy -= 30
@@ -98,10 +126,10 @@ window.addEventListener('load', function (){
             this.speed1 = 1
             this.width2 = 903
             this.height2 = 260
-            this.speed2 = 2
+            this.speed2 = speed * 2
             this.width3 = 1280
             this.height3 = 79
-            this.speed3 = 3
+            this.speed3 = speed * 3
         }
         draw(context){
             context.drawImage(this.image1, this.x1, this.y1, this.width1, this.height1)
@@ -126,28 +154,92 @@ window.addEventListener('load', function (){
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth
             this.gameHeight = gameHeight
+            this.width = 60
+            this.height = 74
+            this.image = document.getElementById('obsteclesImage')
+            this.x = this.gameWidth
+            this.y = this.gameHeight - this.height
+            this.speed = 2
+            this.markedForDeletion = false
+        }
+        draw(context){
+            context.strokeStyle = 'white'
+            context.strokeRect(this.x, this.y, this.width, this.height)
+            //context.fillStyle = 'white'
+            //context.fillRect(this.x, this.y, this.width, this.height)
+            context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+        }
+        update(){
+            this.x -= this.speed
+            if (this.x < 0 - this.width) {
+                this.markedForDeletion = true
+            }
         }
     }
 
-    function handleObstecles(){
-
+    function handleObstecles(deltaTime){
+        if (obstecleTimer > obstecleInterval + randomObstecleInterval){
+            obstecles.push(new Obstecles(canvas.width, canvas.height))
+            console.log(obstecles)
+            //randomObstecleInterval = Math.random() * 1000 + 500
+            obstecleTimer = 0
+        } else {
+            obstecleTimer += deltaTime
+        }
+        obstecles.forEach(obstecle => {
+            obstecle.draw(ctx)
+            obstecle.update()
+        })
+        obstecles = obstecles.filter(obstecles => !obstecles.markedForDeletion)
     }
 
-    function displayStatusText(){
+    function displayStatusText(context){
+        context.fillStyle = 'black'
+        context.font = '40px Helvetica'
+        context.fillText('Score: ' + Math.floor(score), 20, 50)
+        context.fillStyle = 'white'
+        context.font = '40px Helvetica'
+        context.fillText('Score: ' + Math.floor(score), 22, 51)
+    }
 
+    function gameover(context){
+        if(score === 0) {
+            if(gameOver) {
+                context.textAlign = 'center'
+                context.fillStyle = 'black'
+                context.font = '40px Helvetica'
+                context.fillText('Game Over', 500, 300)
+                context.fillStyle = 'white'
+                context.font = '40px Helvetica'
+                context.fillText('Game Over', 502, 301)
+            }
+        }
     }
 
     const input = new InputHandler()
     const player = new Player(canvas.width, canvas.height)
     const background = new Background(canvas.width, canvas.height)
+    const obstecle1 = new Obstecles(canvas.width, canvas.height)
 
-    function animate(){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+    let lastTime = 0
+    let obstecleTimer = 0
+    let obstecleInterval = 4000
+    let randomObstecleInterval = Math.random() * 1500
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime
+        lastTime = timeStamp
+        //console.log(deltaTime)
+        //ctx.clearRect(0,0,canvas.width,canvas.height)
         background.draw(ctx)
         background.update()
         player.draw(ctx)
-        player.update(input)
-        requestAnimationFrame(animate)
+        player.update(input, deltaTime, obstecles)
+        handleObstecles(deltaTime)
+        displayStatusText(ctx)
+        gameover(ctx)
+        if(!gameOver) requestAnimationFrame(animate)
+        if (!(speed === 0)) score += 0.01
     }
-    animate()
+    animate(0)
 })
